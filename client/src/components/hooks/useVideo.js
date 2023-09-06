3a54dde20b35e59b0724f9fe10aa0f55732f93a5
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-09-06 10:57:32 +0530

    create useVideo hook

---
+++ client/src/components/hooks/useVideo.js
@@ -0,0 +1,25 @@
+// useVideo.js
+import { useState, useEffect } from "react";
+
+const useVideo = (vID) => {
+  const [video, setVideo] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getVideoDetails();
+  }, []);
+  const getVideoDetails = async () => {
+    try {
+      const response = await fetch("http://localhost:3333/api/videos/find/" + vID);
+      const data = await response.json();
+      setVideo(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching video details:", error);
+      setLoading(false);
+    }
+  };
+
+  return { video, loading };
+};
+
+export default useVideo;
