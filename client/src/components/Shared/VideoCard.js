44cd59270051576138f2dc9ba59bce52ab5230c6
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-09-19 11:28:41 +0530

    change video card design

--- client/src/components/Shared/VideoCard.js
+++ client/src/components/Shared/VideoCard.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import userContext from "../Helpers/UserContext";
 
 const VideoCard = ({ video }) => {
+  const { user } = useContext(userContext);
+
   return (
-    <> 
-    <div className="bg-[#222f46] hover:bg-slate-700 rounded-2xl p-4 cursor-pointer w-full">
-      <div className="mb-4">
-        <img src={video.imgUrl} alt={video.title} className=" object-fill w-full h-40 rounded-lg" />
+    <>
+      <div className="bg-[#222f46] hover:bg-slate-700 rounded-2xl p-4 cursor-pointer w-full">
+        <div className="mb-4">
+          <img
+            src={video.imgUrl}
+            alt={video.title}
+            className=" object-fill w-full h-40 rounded-lg"
+          />
+        </div>
+        <div className="flex gap-3 w-full">
+          <img className="border-2 rounded-full h-10 w-10 border-[#396983]" src={video.user.imgUrl}/>
+          <div className="flex flex-col gap-0">
+            <span className="text-md text-gray-200 font-semibold">
+              {video.title}
+            </span>
+            <span className="text-sm text-gray-200 font-regular">
+              {video.user.name}
+            </span>
+            <span className="text-sm text-gray-300 font-regular">
+              {video.views} views
+            </span>
+          </div>
+        </div>
       </div>
-      <div className=''><img src=""/></div>
-      <h1 className="text-xl font-semibold">{video.title}</h1>
-      </div>
-   </>
+    </>
   );
 };
 export default VideoCard;
