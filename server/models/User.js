99c22ee5a108f05560a1630ec78a13454a16d1bb
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-09-19 11:27:15 +0530

    added profile image url

--- server/models/User.js
+++ server/models/User.js
@@ -1,35 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    img:{
-        type:String,
+    img: {
+      type: String,
     },
-    videos:{
-        type:[String]
+    videos: {
+      type: [String],
     },
-    subscribers:{
-        type:Number,
-        default:0,
+    subscribers: {
+      type: Number,
+      default: 0,
     },
-    subscribedChannels:{
-        type:[String]
-    }
-},{timestamps:true});
+    subscribedChannels: {
+      type: [String],
+    },
+    profileImageUrl: {
+      type: String,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
