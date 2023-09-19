39b2a313e0ddd56cd7be2ad26e73f53745b5e52c
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-09-19 11:24:05 +0530

    added custom tailwind config colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkblue1: "#02142e",
+        darkblue2: "#101b31",
+        lightblue1: "#24324b",
+        lightblue2: "#30415e"
+      }
+    },
   },
   plugins: [],
 }
