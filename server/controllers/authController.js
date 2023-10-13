4c3bc10631bb9b52acf2df10cbe6845a7030473a
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-10-13 15:58:18 +0530

    package upgrade

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 // async function signup(req, res) {
 //   const salt = bcrypt.genSaltSync(10);
