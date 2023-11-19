a35e991d7777496477f0529b49d267de3f846c7f
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-11-19 11:02:04 +0530

    added forgot password link

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,20 +2,26 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://blue-violet-antelope-wrap.cyclic.app/api/auth', // Adjust this to match your backend API URL
+  baseURL: 'http://localhost:3000/api/auth', // Adjust this to match your backend API URL
   withCredentials: true
 });
 
 export const registerUser = async (credentials) => {
-  const response = await api.post('/signup', credentials);
-  return response;
+  return await api.post('/signup', credentials);
 };
 
 export const loginUser = async (credentials) => {
-  const response = await api.post('/signin', credentials);
-  return response;
+  return await api.post('/signin', credentials);
 };
 
 export const logoutUser = async () => {
   return await api.post('/logout');
 }
+
+export const sendEmailForForgotPassword = async (email) => {
+  return await api.post('/forgotpassword', email);
+}
+
+export const resetPassword = async (token, data) => {
+  return await api.post(`/password/reset/${token}`, data);
+};
