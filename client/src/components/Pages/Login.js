41d7ba2f4a920431715b4e05f9b4e015fd6e73c4
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-11-19 11:02:06 +0530

    added forgot password link page

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { loginUser } from "../../api/auth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../slices/authSlice"
-import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
 
 export default function Login() {
@@ -50,6 +49,8 @@ export default function Login() {
         password: response.data.user.password,
         profileImageUrl: response.data.user.profileImageUrl
       };
+      //set credentials in the local storage
+      localStorage.setItem("userData", JSON.stringify(userData));
       // Check if the "access_token" cookie already exists
       const accessToken = Cookies.get("access_token");
 
@@ -61,16 +62,13 @@ export default function Login() {
           sameSite: "None",
          });
       }
-      // Store user data in localStorage
-      localStorage.setItem("userData", JSON.stringify(userData));
-      dispatch(login(userData));
-
       Cookies.set("access_token", token, {
         expires: 12000000,
         secure: true,
-        sameSite: "None"
-      });
-
+        sameSite: "None",
+       });
+      dispatch(login(userData));
+      
       console.log("Headers : " + response.headers);
       console.log("User : " + JSON.stringify(userData));
       console.log("Cookies : ", Cookies.get("access_token"));
@@ -84,6 +82,17 @@ export default function Login() {
     }
   };
 
+  useEffect(() => {
+    const accessToken = Cookies.get("access_token");
+    const userDataString = localStorage.getItem("userData");
+
+    if (accessToken && userDataString) {
+      const userData = JSON.parse(userDataString);
+      // Dispatch the login action with user data
+      dispatch(login(userData));
+    }
+  }, [dispatch]);
+
   return (
     <div className="flex w-full items-center justify-center min-h-screen ">
       <div className="w-full mx-6 p-6 sm:w-9/12 md:w-3/12 border border-gray-700 rounded-lg shadow-md bg-[#222f46]">
@@ -92,14 +101,6 @@ export default function Login() {
           Login To WIDEO!
         </h2>
         <form className="space-y-4">
-          <div className="mb-4 text-center ">
-            <button className="bg-darkblue2 p-3 mx-auto  text-gray-300 flex justify-center items-center gap-3 rounded-lg focus:outline-none focus:ring focus:border-blue-500">
-              <FcGoogle className="w-6 h-6" /> Google Login
-            </button>
-          </div>
-          <div className="mb-4 text-center text-gray-400 dark:text-gray-400">
-            <span className="bg-[#222f46] px-2 py-1">or</span>
-          </div>
           <div>
             <label htmlFor="name" className="block mb-1 text-sm font-medium text-gray-900 dark:text-slate-400">
               Username
@@ -135,11 +136,12 @@ export default function Login() {
                 Remember Me
               </label>
             </div>
-            <div>
+       
+              <Link to="/forgotpassword">
               <span htmlFor="rememberMe" className="font-semibold text-sm ml-2 text-blue-600 dark:text-blue-500 cursor-pointer hover:text-primary-500">
                 Forgot Password?
               </span>
-            </div>
+          </Link>
 
           </div>
           <button
